3cb1dc5db3211496aa6076b6fa6fdf2ad4b12718
Aucer <nguyenson080301@gmail.com>
2023-08-04 10:17:32 +0700

    NguyenVanSon_Cau2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenVanSon_02.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
